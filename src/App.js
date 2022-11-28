388b8dd097b3327182752178be43e1dbe23333f2
smfahim25 <smfahimhossen@gmail.com>
2022-11-28 12:00:37 +0600

    file added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
@@ -8,12 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <span>
-          <span>Learn </span>
+          <span>Go to hell </span>
           <a
             className="App-link"
             href="https://reactjs.org/"
